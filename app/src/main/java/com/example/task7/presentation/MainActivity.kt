16061716d1e7e6e2a1864850b4e75d7b6f6184bc
Ziban89 <ziban89@tut.by>
2022-03-16 01:31:35 +0300

    init

---
+++ app/src/main/java/com/example/task7/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.task7.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.task7.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
